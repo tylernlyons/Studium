06a6efc64fcb3229639d6be31aad6aaddd9c33f4
Tyler <tylernlyons04@gmail.com>
2025-03-27 10:19:54 -0400

    Added untracked session option

--- termproject4300/src/app/pages/home/page.tsx
+++ termproject4300/src/app/pages/home/page.tsx
@@ -39,6 +39,13 @@ export default function Home() {
         <button onClick={handleSetDuration}>Set Duration</button>
       </div>
 
+      <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <h3>Untracked study session:</h3>
+        <Link href={'/pages/focusMode'}>
+          <button>Start Study Session</button>
+        </Link>
+      </div>
+
       {studyDuration > 0 && (
         <div>
           <h3>Your study session will last for {studyDuration} minute(s)</h3>
