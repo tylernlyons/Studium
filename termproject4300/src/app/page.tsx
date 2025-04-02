df3607792876d64d119f7e33a77fadff3a102811
Tyler <tylernlyons@gmail.com>
2025-04-02 19:24:13 -0400

    Tweaked styling

--- termproject4300/src/app/page.tsx
+++ termproject4300/src/app/page.tsx
@@ -1,11 +1,11 @@
-import { redirect } from "next/navigation";
-
+import "./SplashPage.css"
 //unauthorized home page with about, welcome, etc.
 
 export default function RootPage() {
   return (
-  <div>
-    <h1>"This is the splash page. Log in to begin study session.</h1>
+  <div className="splash-container">
+    <h1>This is the splash page. Log in to begin study session.</h1>
+    <video src="/videos/splash.mp4" autoPlay loop muted></video>
   </div>
   )
 }
--- termproject4300/src/components/Navbar.tsx
+++ termproject4300/src/components/Navbar.tsx
@@ -1,48 +1,45 @@
 'use client';
-// This is from my Nextjs resources, Navbar_basic
+
+import "./Navbar.modules.css"
 import Image from 'next/image';
-import {useState} from 'react';
+import { useState } from 'react';
 import Link from 'next/link';
 
-
 const Navbar = () => {
-
-
-  const [isLoggedIn,setIsLoggedIn ] = useState(false);
-
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
   const handleLogin = () => {
     setIsLoggedIn(prev => !prev);
-  }
+  };
 
   return (
-    <nav className='bg-green-700 border-b-1 border-white'>
-      <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
-        <div className='relative flex h-20'>
-
-         { 
-          !isLoggedIn && (
-                <div className='hidden md:block md:ml-6'>
-                  <div className='flex items-center'>
-                    <Link href={"./pages/home"}>
-                    <button onClick = {handleLogin} className='flex items-center text-white bg-gray-400 hover:bg-gray-500 hover:text-white rounded-md px-3 py-2'>
-                      <span>Login | Register</span>
-                    </button>
-                    </Link>
-                  </div>
-                </div>
-          )
-        }
-        { isLoggedIn && (
-                <div className='hidden md:block md:ml-6'>
-                  <div className='flex items-center'>
-                  <Link href={"/"}>
-                    <button onClick = {handleLogin} className='flex items-center text-white bg-gray-400 hover:bg-gray-500 hover:text-white rounded-md px-3 py-2'>
-                      <span>Logout</span>
-                    </button>
-                    </Link>
-                  </div>
-                </div>
-        )}
+    <nav>
+      <div className='max-w-7xl mx-auto px-2 sm:px-6 lg:px-8'>
+        <div className='name-login-container'>
+          <div className="nav-items">
+            <span className='text-white text-xl font-bold'>Studium</span>
+          </div>
+          <div className='nav-items'>
+            {!isLoggedIn ? (
+              <Link href="./pages/home">
+                <button 
+                  onClick={handleLogin} 
+                  className='nav-items'
+                >
+                  <span>Login | Register</span>
+                </button>
+              </Link>
+            ) : (
+              <Link href="/">
+                <button 
+                  onClick={handleLogin} 
+                  className='nav-items'
+                >
+                  <span>Logout</span>
+                </button>
+              </Link>
+            )}
+          </div>
         </div>
       </div>
     </nav>
