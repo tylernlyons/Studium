02a964d415d42f0a9a94fdf0f5e968bc09a82253
Tyler <tylernlyons@gmail.com>
2025-04-02 20:43:57 -0400

    Added logo

--- termproject4300/src/components/Navbar.tsx
+++ termproject4300/src/components/Navbar.tsx
@@ -17,11 +17,18 @@ const Navbar = () => {
       <div className='max-w-7xl mx-auto px-2 sm:px-6 lg:px-8'>
         <div className='name-login-container'>
           <div className="nav-items">
+            <img className="size-10" src="../images/logobg_studium.png" alt="Studium logo" />
             <span className='text-white text-xl font-bold'>Studium</span>
+            <Link href="/pages/about">
+            <button className='nav-items'>
+              <span>About</span>
+            </button>
+            </Link>
           </div>
+          
           <div className='nav-items'>
             {!isLoggedIn ? (
-              <Link href="./pages/home">
+              <Link href="/pages/home">
                 <button 
                   onClick={handleLogin} 
                   className='nav-items'
