a296d9f7327aaa5c4c021954be2d5d9376138a8b
Tyler Lyons <tylernlyons04@gmail.com>
2025-04-03 13:03:19 -0400

    Updated styling

--- termproject4300/src/components/Navbar.tsx
+++ termproject4300/src/components/Navbar.tsx
@@ -14,14 +14,13 @@ const Navbar = () => {
 
   return (
     <nav>
-      <div className='max-w-7xl mx-auto px-2 sm:px-6 lg:px-8'>
         <div className='name-login-container'>
           <div className="nav-items">
             <img className="size-10" src="../images/logobg_studium.png" alt="Studium logo" />
             <span className='text-white text-xl font-bold'>Studium</span>
             <Link href="/pages/about">
             <button className='nav-items'>
-              <span>About</span>
+              <span className="span">About</span>
             </button>
             </Link>
           </div>
@@ -33,7 +32,7 @@ const Navbar = () => {
                   onClick={handleLogin} 
                   className='nav-items'
                 >
-                  <span>Login | Register</span>
+                  <span className="span">Login | Register</span>
                 </button>
               </Link>
             ) : (
@@ -42,13 +41,12 @@ const Navbar = () => {
                   onClick={handleLogin} 
                   className='nav-items'
                 >
-                  <span>Logout</span>
+                  <span className="span">Logout</span>
                 </button>
               </Link>
             )}
           </div>
         </div>
-      </div>
     </nav>
   );
 };
