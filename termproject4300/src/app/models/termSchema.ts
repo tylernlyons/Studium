eb62961749aa20317a1a58b72bba9d5016a00548
Tyler Lyons <tylernlyons04@gmail.com>
2025-04-07 15:03:43 -0400

    Defined models

---
+++ termproject4300/src/app/models/termSchema.ts
@@ -0,0 +1,14 @@
+import mongoose, { Schema, Document, Model } from "mongoose";
+
+export interface ITerm extends Document {
+    term: string;
+    definition: string;
+}
+
+const TermSchema = new Schema<ITerm>({
+    term: { type: String, required: true},
+    definition: { type: String, required: true}
+});
+
+export default mongoose.models.Term || mongoose.model<ITerm>("Term", TermSchema);
+
