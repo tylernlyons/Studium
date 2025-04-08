cb76b16a81b855b902983427af96e28910daa2f2
Vivian Aguirre <131201297+vivianyaguirre@users.noreply.github.com>
2025-04-08 13:22:27 -0400

    Combined logo img and title in Navbar.tsx to redirect to home page

--- termproject4300/src/components/Navbar.tsx
+++ termproject4300/src/components/Navbar.tsx
@@ -16,8 +16,14 @@ const Navbar = () => {
     <nav>
         <div className='name-login-container'>
           <div className="nav-items">
-            <img className="size-10" src="../images/logobg_studium.png" alt="Studium logo" />
-            <span className='text-white text-xl font-bold'>Studium</span>
+
+            <Link href="/" className="flex items-center gap-2 hover:opacity-90">
+              <div className="flex items-center bg-gray-800 p-2 rounded-md shadow-md">
+                <img className="size-10" src="../images/logobg_studium.png" alt="Studium logo" />
+                <span className='text-white text-xl font-bold ml-2'>Studium</span>
+              </div>
+              </Link>
+              
             <Link href="/pages/about">
             <button className='nav-items'>
               <span className="span">About</span>
