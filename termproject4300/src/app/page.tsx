10183a3332aeacd128fab3505a355869e16791d7
Vivian Aguirre <131201297+vivianyaguirre@users.noreply.github.com>
2025-04-09 10:53:08 -0400

    Styling home page
    Styling Home Page

--- termproject4300/src/app/page.tsx
+++ termproject4300/src/app/page.tsx
@@ -5,14 +5,60 @@ import connectMongoDB from "../../config/mongodb";
 export default function RootPage() {
   connectMongoDB();
   return (
-    <div className="splash-container">
+    
+    <><div className="splash-container">
+
       <div className="intro">
-        <h1>STUDIUM</h1>
-        <p className="p">A distraction-free study platform with a built-in lockdown mode, AI-powered assistance, and customizable flashcards. Stay focused, track your sessions, and power through your studies!</p>
-      </div>
-      <div>
+        <div className="intro-content">
+          <h1>STUDIUM</h1>
+          <p className="p">A distraction-free study platform with a built-in lockdown mode, AI-powered assistance, and customizable flashcards. Stay focused, track your sessions, and power through your studies!</p>
+          <button>Start your session here</button>
+        </div>
+
+        <div className="intro-img">
+          <img src="/images/lofi.jpg" alt="Focus Studying img"></img>
+        </div>
 
       </div>
     </div>
-  )
+
+    <div className="features-container">
+        <h2 className="section-heading">Platform Features</h2>
+        <div className="three-col">
+          <div className="card">
+            <img src="/images/organize.jpeg" alt="Organize" />
+            <h3>Organize Your Study Sets</h3>
+            <ul>
+              <li>
+                Create and name your own study sets! The sidebar navigation makes it easy to access whenever you need them.
+              </li>
+            </ul>
+          </div>
+
+          <div className="card">
+            <img src="/images/lockdown.png" alt="Lockdown Mode" />
+            <h3>Lockdown Mode</h3>
+            <ul>
+              <li>
+                Focus Mode helps you stay on task. When activated, Focus Mode locks your session for a set amount of time.
+              </li>
+            </ul>
+          </div>
+
+          <div className="card">
+            <img src="/images/studysmart.png" alt="Study Smarter" />
+            <h3>Study Smarter with AI</h3>
+            <ul>
+              <li>
+                AI-powered suggestions provide personalized studying. Your session history keeps track of how long you studied.
+              </li>
+            </ul>
+          </div>
+
+
+        </div>
+
+    </div></>
+      
+    )
 }
