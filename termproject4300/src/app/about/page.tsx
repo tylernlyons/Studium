c7edc0d610876a9358793e457ebd8583e06b4eb9
Hannah Le <hannahle5275@gmail.com>
2025-04-09 16:13:20 -0400

    Enhance About Page

--- termproject4300/src/app/about/page.tsx
+++ termproject4300/src/app/about/page.tsx
@@ -7,10 +7,10 @@ export default function about() {
 
     return (
         <div className="about px-6 py-10">
-            <div className="flex flex-col lg:flex-row items-center justify-between gap-4 lg:gap-4 mb-4 lg:mb-0">
+            <div className="flex flex-col lg:flex-row items-center justify-between gap-4 lg:gap-4 mb-4 lg:mb-0 text-center">
                 <div className="flex-1">
-                    <h2 className="about-title text-2xl font-bold mb-4">About Us</h2>
-                    <p className="about-description">
+                    <h2 className="about-title text-4xl font-bold mb-4">About Us</h2>
+                    <p className="about-description text-lg">
                         At Studium, we believe that focused minds create powerful futures. In a world of endless distractions, we set out to build a study platform that prioritizes <span className="highlight">concentration</span>, <span className="highlight">integrity</span>, and <span className="highlight">smart learning</span>.
                     </p>
                 </div>
@@ -26,43 +26,43 @@ export default function about() {
                 </div>
             </div>
                 
-            <div className="flex flex-col lg:flex-row items-center justify-between gap-10 mb-16 lg:ml-15 lg:mr-10">
+            <div className="flex flex-col lg:flex-row items-center justify-between gap-10 mb-16 lg:ml-10 lg:mr-10">
                 <div className="flex-1">
                     <Image
                     className="rounded-md shadow-lg"
                     src="/images/about-img2.jpg"
                     alt="About Image 2"
-                    width={400}
-                    height={200}
+                    width={450}
+                    height={400}
                     />
                 </div>
 
                 <div className="flex-1 text-center lg:text-left mt-4">
-                    <h2 className="about-title text-2xl font-bold mb-4">Our Mission</h2>
-                    <ul className="space-y-6 text-base about-description">
+                    <h2 className="about-title text-4xl font-bold mb-4 text-center">Our Mission</h2>
+                    <ul className="space-y-6 text-base about-description text-center">
                         <li>
                             <p className="feature-heading">Lockdown Mode</p>
-                            <p>Helping users stay focused by limiting distractions and browser access during study sessions.</p>
+                            <p className="text-lg">Helping users stay focused by limiting distractions and browser access during study sessions.</p>
                         </li>
 
                         <li>
                             <p className="feature-heading">Custom Study Sets</p>
-                            <p>Allowing learners to build, organize, and revisit personalized flashcards and material.</p>
+                            <p className="text-lg">Allowing learners to build, organize, and revisit personalized flashcards and material.</p>
                         </li>
 
                         <li>
                             <p className="feature-heading">AI-Powered Study Tools</p>
-                            <p>Offering AI recommendations and dynamic content tailored to your needs.</p>
+                            <p className="text-lg">Offering AI recommendations and dynamic content tailored to your needs.</p>
                         </li>
 
                         <li>
                             <p className="feature-heading">Focus Mode</p>
-                            <p>Encouraging study habits by locking sessions for a set period of uninterrupted learning.</p>
+                            <p className="text-lg">Encouraging study habits by locking sessions for a set period of uninterrupted learning.</p>
                         </li>
 
                         <li>
                             <p className="feature-heading">Progress Tracking</p>
-                            <p>Keeping a history of study sessions so users can see their growth and patterns.</p>
+                            <p className="text-lg">Keeping a history of study sessions so users can see their growth and patterns.</p>
                         </li>
                     </ul>
                 </div>
