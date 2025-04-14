064fb68b69b2969503ba5ba1a23edacc5d4ead39
Vivian Aguirre <131201297+vivianyaguirre@users.noreply.github.com>
2025-04-14 13:34:38 -0400

    Styling Timer page

--- termproject4300/src/app/home/page.tsx
+++ termproject4300/src/app/home/page.tsx
@@ -28,32 +28,49 @@ export default function Home() {
   };
 
   return (
-
-    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div>
-        <h2>Set Your Study Duration</h2>
+    <div
+      className="h-screen w-screen flex items-center justify-center text-white"
+      style={{
+        background: "radial-gradient(circle at center, rgba(71, 121, 196, 1) 0%, rgba(2, 0, 36, 1) 100%)"
+      }}
+    >
+      <div className="backdrop-blur-md bg-white/10 border border-white/20 shadow-2xl rounded-3xl p-10 flex flex-col items-center w-[90%] max-w-md space-y-6">
+        
+      
+        <h2 className="text-3xl font-bold text-white-700 mb-8">Set Your Study Duration</h2>
+      
         <input
           type="number"
           value={durationInput}
           onChange={(e) => setDurationInput(e.target.value)}
           placeholder="Enter time in minutes"
+          className="text-white text-center px-10 py-2 rounded-lg w-65 outline-none"
         />
-        <button onClick={handleSetDuration}>Set Duration</button>
-      </div>
 
-      {studyDuration > 0 && (
-        <div>
-          <h3>Your study session will last for {studyDuration} minute(s)</h3>
-          <Link href={getFocusModeUrl()}>
-            <button>Start Study Session</button>
+        
+        <button
+          onClick={handleSetDuration}
+          className="bg-blue-500 hover:bg-blue-600 transition text-white px-6 py-2 rounded-full"
+        >
+          Set Duration
+        </button>
+
+        {studyDuration > 0 && (      
+        <Link href={getFocusModeUrl()}>
+            <button className="bg-green-500 text-white justify-center px-6 py-2 rounded shadow hover:bg-green-600 transition">
+              Start Study Session</button>
+        </Link>
+        )}
+
+        {/* Untimed Option */}
+        <div className="text-center pt-2">
+          <p className="text-sm text-white/80 mb-1">Or start an untimed session:</p>
+          <Link href="/focusMode">
+            <button className="bg-blue-400 text-white px-6 py-2 rounded-full hover:bg-blue-500 transition">
+              Start Study Session
+            </button>
           </Link>
         </div>
-      )}
-      <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <h3>Untimed study session:</h3>
-        <Link href={'/focusMode'}>
-          <button>Start Study Session</button>
-        </Link>
       </div>
     </div>
   );
