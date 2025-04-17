a1a5a4090a42d66d29c1f8404ea5d526c2db3183
Tyler <tylernlyons04@gmail.com>
2025-04-17 10:06:05 -0400

    Added timer

--- termproject4300/src/app/flashcard-view/[id]/page.tsx
+++ termproject4300/src/app/flashcard-view/[id]/page.tsx
@@ -3,35 +3,33 @@
 import { useState, useEffect } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import FlashcardViewer from '@/components/Flashcard';
+import StudyTimer from '@/components/StudyTimer';
 import Image from 'next/image';
+import { StudySetProps } from '@/components/StudySet';
 
 export default function FlashcardPage() {
+    const [studySet, setStudySet] = useState<StudySetProps['set'] | null>(null);
     const { id } = useParams();
     const router = useRouter();
-
-    const [studySet, setStudySet] = useState({
-        title: '',
-        url: '',
-        terms: [] as { term: string; definition: string }[],
-    });
-
-    useEffect(() => {
-        if (!id) return;
-        const fetchStudySet = async () => {
-            const res = await fetch(`/api/studysets/${id}`);
-            const data = await res.json();
-            setStudySet({
-                title: data.studySet.title,
-                url: data.studySet.url,
-                terms: data.studySet.terms || [],
-            });
-        };
-        fetchStudySet();
+  
+    useEffect(() => { 
+      if (!id) return;
+      const fetchStudySet = async () => { //fetches the supplied studyset from Mongo
+        const res = await fetch(`/api/studysets/${id}`);
+        const data = await res.json();
+        setStudySet(data.studySet);
+      };
+      fetchStudySet();
     }, [id]);
+  
+    if (!studySet) { //Shows loading, while fetching set
+      return <div className='text-center text-xl p-10'>Loading...</div>;
+    }
 
     return (
         <div className="min-h-screen flex flex-col items-center justify-center px-4 py-6">
-            {studySet.url && (
+            <StudyTimer />
+            {studySet.url && ( //if there's a valid image URL for the set, set it
                 <Image
                     src={studySet.url}
                     alt={studySet.title}
@@ -40,6 +38,7 @@ export default function FlashcardPage() {
                     className="object-cover rounded-md"
                 />
             )}
+            {!studySet.url && <p>No image available</p>}
             <h1 className="text-2xl font-bold mb-4">{studySet.title}</h1>
             <FlashcardViewer
                 terms={studySet.terms}
--- termproject4300/src/app/show-set/[id]/page.tsx
+++ termproject4300/src/app/show-set/[id]/page.tsx
@@ -1,21 +1,21 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { useParams, useRouter } from 'next/navigation';
+import { useParams } from 'next/navigation';
 import Card from '@/components/Card';
 import Image from 'next/image';
 import Link from 'next/link';
 import { StudySetProps } from '@/components/StudySet';
 import HoverDefinition from '@/components/HoverDefinition';
+import StudyTimer from '@/components/StudyTimer';
 
 export default function ShowStudySet() {
-  const router = useRouter();
   const [studySet, setStudySet] = useState<StudySetProps['set'] | null>(null);
   const { id } = useParams();
 
-  useEffect(() => {
+  useEffect(() => { 
     if (!id) return;
-    const fetchStudySet = async () => {
+    const fetchStudySet = async () => { //fetches the supplied studyset from Mongo
       const res = await fetch(`/api/studysets/${id}`);
       const data = await res.json();
       setStudySet(data.studySet);
@@ -23,12 +23,13 @@ export default function ShowStudySet() {
     fetchStudySet();
   }, [id]);
 
-  if (!studySet) {
-    return <div>Loading...</div>;
+  if (!studySet) { //Shows loading, while fetching set
+    return <div className='text-center text-xl p-10'>Loading...</div>;
   }
 
   return (
     <div className="bg-white min-h-screen text-black py-10">
+      <StudyTimer />
       <Link
         href="/focusMode"
         className="inline-block px-4 py-2 border border-gray-500 text-red-700 hover:bg-red-800 hover:text-white transition rounded"
@@ -38,7 +39,7 @@ export default function ShowStudySet() {
       <div className="max-w-5xl mx-auto px-4">
         <Card>
           <div className="w-full h-72 relative">
-            {studySet.url && (
+            {studySet.url && ( //if there's a valid image URL for the set, set it
               <Image
                 src={studySet.url}
                 alt={studySet.title}
@@ -46,13 +47,13 @@ export default function ShowStudySet() {
                 className="object-cover rounded-md"
               />
             )}
-            {!studySet.url && <p>No image available</p>}
+            
           </div>
 
           <div className="mt-4 space-y-2">
             <h2 className="text-xl font-semibold">{studySet.title}</h2>
             <ul>
-              {studySet.terms.map((t, i) => (
+              {studySet.terms.map((t, i) => ( //map the terms of the study set to an unordered list
                 <li key={i}>
                   <strong>{t.term}</strong>: {t.definition}
                 </li>
@@ -60,13 +61,13 @@ export default function ShowStudySet() {
             </ul>
 
             <Link
-              href={`/update-set/${id}`}
+              href={`/update-set/${id}`} //sends user to edit the current set
               className="w-full px-6 py-2 border border-gray-500 text-red-700 hover:bg-red-700 hover:text-white transition rounded text-center"
             >
               Edit set
             </Link>
             <Link
-              href={`/flashcard-view/${id}`}
+              href={`/flashcard-view/${id}`} //sends user to view flashcards of current set
               className="w-full px-6 py-2 border border-gray-500 text-red-700 hover:bg-red-700 hover:text-white transition rounded text-center"
             >
               Start flashcards
